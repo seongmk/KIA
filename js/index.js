54ede38bf21c21eb2f9737367541b336b1b3934e
seongmk <64449248+seongmk@users.noreply.github.com>
2020-05-20 15:46:43 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -7,3 +7,14 @@ $(function(){
       $('.skip_menu').removeClass('active');
     });
   })
+
+  // 토글네비
+  var tgl_nav=$('.large_nav>.content>ul:frist-child>li>a');
+  tgl_nav.click(function(){
+    if($(this).parent().hasClass('active')){
+      tgl_nav.parent().removeClass('active');
+    }else{
+      tgl_nav.parent().removeClass('active');
+      $(this).parent().addClass('active');
+    }
+  });
